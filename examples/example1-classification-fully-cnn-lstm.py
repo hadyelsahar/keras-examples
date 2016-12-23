517eb9ae2cafd31fa2a4be17653e11a65f9bab31
hadyelsahar <hadyelsahar@gmail.com>
2016-12-23 05:59:47 +0100

    initial commit binary classification using a fully connected neural network

--- examples/example1-classification-fully-cnn-lstm.py
+++ examples/example1-classification-fully-cnn-lstm.py
@@ -29,6 +29,7 @@ X_test = sequence.pad_sequences(X_test, maxlen=max_seq_length)
 #########################
 # model1 : one layer NN #
 #########################
+# model http://machinelearningmastery.com/binary-classification-tutorial-with-the-keras-deep-learning-library/
 model = Sequential()
 X_fully_tain = np.expand_dims(X_train, axis=1)
 X_fully_test = np.expand_dims(X_test, axis=1)
@@ -38,11 +39,12 @@ model.add(Dense(output_dim=64, input_dim=max_seq_length))
 model.add(Dense(12, input_dim=8, init='uniform', activation='relu'))
 model.add(Dense(8, init='uniform', activation='relu'))
 model.add(Dense(1, init='uniform', activation='sigmoid'))
-model.compile(loss='categorical_crossentropy', optimizer=SGD(lr=0.01, momentum=0.9, nesterov=True))
+model.compile(loss='binary_crossentropy', optimizer=SGD(lr=0.01, momentum=0.9, nesterov=True))
 
 model.fit(X_train, y_train, batch_size=32, nb_epoch=10)
 loss_and_metrics = model.evaluate(X_test, y_test, batch_size=32)
 print loss_and_metrics
+
 
 
 
