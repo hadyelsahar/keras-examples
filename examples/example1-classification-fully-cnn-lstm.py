996efba63f853c6085fa79c6fcbb82724af3a3f2
hadyelsahar <hadyelsahar@gmail.com>
2016-12-24 04:20:07 +0100

    Add lstm , lstm embedding , BIdirec lstm

--- examples/example1-classification-fully-cnn-lstm.py
+++ examples/example1-classification-fully-cnn-lstm.py
@@ -1,10 +1,11 @@
 import numpy as np
 from keras.datasets import imdb
 from keras.models import Sequential
-from keras.layers import Dense, Activation, LSTM, Conv1D
+from keras.layers import Dense, Activation, LSTM, Conv1D, MaxPooling1D, Dropout
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 from keras.optimizers import SGD
+from gensim.models.word2vec import Word2Vec
 
 
 
@@ -16,36 +17,146 @@ np.random.seed(7)
 ####################
 # IMDB Sentiment Classification
 # load the dataset but only keep the top n words, zero the rest
-top_words = 5000
-max_seq_length = 500
-embedding_vector_length = 150
+TOP_WORDS = 5000
+MAX_SEQ_LENGTH = 500
+EMB_VEC_LENGTH = 150
+N_EPOCHS = 10
 
 
-(X_train, y_train), (X_test, y_test) = imdb.load_data(nb_words=top_words)
-X_train = sequence.pad_sequences(X_train, maxlen=max_seq_length)
-X_test = sequence.pad_sequences(X_test, maxlen=max_seq_length)
-
+(X_train, y_train), (X_test, y_test) = imdb.load_data(nb_words=TOP_WORDS)
+X_train = sequence.pad_sequences(X_train, maxlen=MAX_SEQ_LENGTH)
+X_test = sequence.pad_sequences(X_test, maxlen=MAX_SEQ_LENGTH)
 
 #########################
 # model1 : one layer NN #
 #########################
 # model http://machinelearningmastery.com/binary-classification-tutorial-with-the-keras-deep-learning-library/
-model = Sequential()
-X_fully_tain = np.expand_dims(X_train, axis=1)
-X_fully_test = np.expand_dims(X_test, axis=1)
-# model.add(Embedding(top_words, embedding_vector_length, input_length=max_seq_length))
 
-model.add(Dense(output_dim=64, input_dim=max_seq_length))
+model = Sequential()
+model.add(Dense(output_dim=64, input_dim=MAX_SEQ_LENGTH))
 model.add(Dense(12, input_dim=8, init='uniform', activation='relu'))
 model.add(Dense(8, init='uniform', activation='relu'))
 model.add(Dense(1, init='uniform', activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer=SGD(lr=0.01, momentum=0.9, nesterov=True))
 
-model.fit(X_train, y_train, batch_size=32, nb_epoch=10)
-loss_and_metrics = model.evaluate(X_test, y_test, batch_size=32)
-print loss_and_metrics
+model.fit(X_train, y_train, batch_size=32, nb_epoch=N_EPOCHS)
+scores = model.evaluate(X_test, y_test, batch_size=32)
+print("Accuracy: %.2f%%" % (scores[1]*100))
 
+#################
+# model2 : LSTM #
+#################
+# model from : http://machinelearningmastery.com/sequence-classification-lstm-recurrent-neural-networks-python-keras/
+# more about LSTM :
+# http://deeplearning.net/tutorial/lstm.html
+# http://colah.github.io/posts/2015-08-Understanding-LSTMs/
 
+# create the model
+model = Sequential()
+model.add(Embedding(TOP_WORDS, EMB_VEC_LENGTH, input_length=MAX_SEQ_LENGTH))
+model.add(LSTM(100))
+model.add(Dense(1, activation='relu'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
 
+model.fit(X_train, y_train, nb_epoch=N_EPOCHS, batch_size=64)
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
 
+# ######################
+# # model3: CNN + LSTM #
+# ######################
+EMB_VEC_LENGTH = 32
+model = Sequential()
+model.add(Embedding(TOP_WORDS, EMB_VEC_LENGTH, input_length=MAX_SEQ_LENGTH))
+model.add(Conv1D(nb_filter=32, filter_length=3, activation='relu', border_mode='same'))
+model.add(MaxPooling1D(pool_length=2))
+model.add(Dropout(0.2))
+model.add(LSTM(100))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+print model.summary()
+model.fit(X_train, y_test, batch_size=64, nb_epoch=N_EPOCHS)
+
+scores = model.evaluate(X_test, y_test)
+print("Accuracy: %.2f%%" % (scores[1]*100))
+
+#################################
+# model3: CNN + LSTM + Word2vec #
+#################################
+
+word_vectors = Word2Vec.load_word2vec_format('../data/wordvectors/GoogleNews-vectors-negative300.bin.gz', binary=True)
+index_dict = imdb.get_word_index()
+
+embedding_weights = np.zeros((TOP_WORDS + 1, word_vectors.vector_size))
+for word, index in index_dict.items():
+    if word in word_vectors and index <= TOP_WORDS:
+        embedding_weights[index, :] = word_vectors[word]
+
+model = Sequential()
+model.add(Embedding(TOP_WORDS + 1, word_vectors.vector_size, input_length=MAX_SEQ_LENGTH, weights=[embedding_weights]))
+model.add(Conv1D(nb_filter=32, filter_length=3, activation='relu', border_mode='same', input_shape=[MAX_SEQ_LENGTH, word_vectors.vector_size]))
+model.add(MaxPooling1D(pool_length=2))
+model.add(Dropout(0.2))
+model.add(LSTM(100))
+model.add(Dense(1, activation='relu'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+print model.summary()
+model.fit(X_train, y_test, batch_size=64, nb_epoch=N_EPOCHS)
+
+scores = model.evaluate(X_test, y_test)
+print("Accuracy: %.2f%%" % (scores[1]*100))
+
+#################################
+# model4: LSTM + Word2vec #
+#################################
+
+word_vectors = Word2Vec.load_word2vec_format('../data/wordvectors/GoogleNews-vectors-negative300.bin.gz', binary=True)
+index_dict = imdb.get_word_index()
+
+embedding_weights = np.zeros((TOP_WORDS + 1, word_vectors.vector_size))
+for word, index in index_dict.items():
+    if word in word_vectors and index <= TOP_WORDS:
+        embedding_weights[index, :] = word_vectors[word]
+
+model = Sequential()
+model.add(Embedding(TOP_WORDS + 1, word_vectors.vector_size, input_length=MAX_SEQ_LENGTH, weights=[embedding_weights]))
+model.add(LSTM(100))
+model.add(Dropout(0.2))
+model.add(Dense(1, activation='relu'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+print model.summary()
+model.fit(X_train, y_test, batch_size=64, nb_epoch=N_EPOCHS)
+
+scores = model.evaluate(X_test, y_test)
+print("Accuracy: %.2f%%" % (scores[1]*100))
+
+#################################
+# model5: BidirectionalLSTM + Word2vec #
+#################################
+
+word_vectors = Word2Vec.load_word2vec_format('../data/wordvectors/GoogleNews-vectors-negative300.bin.gz', binary=True)
+index_dict = imdb.get_word_index()
+
+embedding_weights = np.zeros((TOP_WORDS + 1, word_vectors.vector_size))
+for word, index in index_dict.items():
+    if word in word_vectors and index <= TOP_WORDS:
+        embedding_weights[index, :] = word_vectors[word]
+
+model = Sequential()
+model.add(Embedding(TOP_WORDS + 1, word_vectors.vector_size, input_length=MAX_SEQ_LENGTH, weights=[embedding_weights]))
+model.add(LSTM(100))
+# model.add(Dropout(0.2))
+model.add(Dense(1, activation='relu'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+print model.summary()
+model.fit(X_train, y_test, batch_size=64, nb_epoch=N_EPOCHS)
+
+scores = model.evaluate(X_test, y_test)
+print("Accuracy: %.2f%%" % (scores[1]*100))
 
