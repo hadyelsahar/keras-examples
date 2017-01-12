5d148ab75dd103fe8262c5eb2ea8abff0493f734
hadyelsahar <hadyelsahar@gmail.com>
2017-01-12 14:51:10 +0100

    remove fmeasure

--- examples/example2-seq2seq-RE.py
+++ examples/example2-seq2seq-RE.py
@@ -45,7 +45,7 @@ model.add(Embedding(TOP_WORDS, EMB_VEC_LENGTH, input_length=MAX_SEQ_LENGTH))
 model.add(LSTM(100, dropout_U=-0.2, dropout_W=0.2))
 model.add(Dense(len(y[0]), activation='relu'))
 sgd = SGD(lr=0.01, clipnorm=1.)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy', 'fmeasure'])
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 print(model.summary())
 
 model.fit(X_train, y_train, nb_epoch=N_EPOCHS, batch_size=64)
